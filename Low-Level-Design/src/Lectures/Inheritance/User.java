0dc52c9a92d80641470c8e95872530f7799aa9fd
Suvi05 <suv143ak@gmail.com>
2023-09-23 08:44:26 +0530

    Changes done in Inheritance package - Lecture -2 Scaler Class

--- Low-Level-Design/src/Lectures/Inheritance/User.java
+++ Low-Level-Design/src/Lectures/Inheritance/User.java
@@ -5,6 +5,8 @@ public class User {
     String email;
     String password;
 
+    private String test; //We can not directly access this in other classes, but using setters and getters we can!
+
     void saySomething() {
         System.out.println("Say Something");
     }
